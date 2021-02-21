80624067868620bb33024fe47d69b8df576f8b55
bilalkhann16 <lalbikhan2014@gmail.com>
2021-02-22 03:19:32 +0500

    push1

---
+++ myapp_output/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyappOutputConfig(AppConfig):
+    name = 'myapp_output'
---
+++ myapp_output/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import re
+import json
+from urllib.request import urlopen
+
+# key: AKIAIDOF3JTWMWSD3SEQ 
+# secret: 2/lxtJ4vBOo6EN9hH5X83QXbn1t1jqmqn9oP1uFC 
+
+
+# Create your views here.
+def myView(request):
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+
+    IP=data['ip']
+    org=data['org']
+    org= org[7:]
+    city = data['city']
+    country=data['country']
+    region=data['region']
+
+    print ('Your IP detail\n ')
+    print ('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
+
+    #return HttpResponse('hello world')
+    return render(request, 'index.html', {'IP':IP,'org':org,'city': city, 'country': country, 'region':region})
